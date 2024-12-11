b54fa682e8c3ec082ffe831091486ca6a734c971
Guga <guga.kobakhidze@makingscience.com>
2024-12-11 06:00:13 +0400

    Add experiance section

--- src/components/Experience/style.ts
+++ src/components/Experience/style.ts
@@ -1,106 +1,88 @@
-import { Grid2, styled, Typography } from "@mui/material";
+import { Box, Grid2, styled } from "@mui/material";
 
 export const StyledImage = {
   userSelect: "none",
   pointerEvents: "none",
 };
 
-const styles = {
-  border: "0.5px solid #1E7E9933",
-  backgroundColor: "#1C1C1C",
-  borderRadius: "24px",
+const orderListBefore = {
+  "&::before": {
+    content: "counter(list-counter) ''",
+    counterIncrement: "list-counter",
+    position: "absolute",
+    left: "0",
+    color: "#fff",
+    fontWeight: 700,
+  },
 };
 
-export const StyledRateBox = styled(Grid2)({
-  ...styles,
-  justifyItems: "center",
-  alignContent: "center",
-  textAlign: "center",
-  padding: "32px 72px",
-  height: "170px",
-
-  p: {
-    fontSize: "12px",
-    fontWeight: 500,
-    lineHeight: "14.4px",
-    letterSpacing: "1px",
-    color: "rgba(255, 255, 255, 0.5)",
-  },
-});
-
-export const StyledRateText = styled(Typography)({
-  background: "linear-gradient(179.95deg, #002F70 6.53%, #00C3FF 81.79%)",
-  WebkitBackgroundClip: "text",
-  color: "transparent",
-  fontSize: "52px",
-  fontWeight: 500,
-  lineHeight: "62.4px",
-
-  span: {
-    marginLeft: "5px",
-  },
-});
-
-export const StyledBiography = styled(Grid2)({
-  ...styles,
+export const StyledExperianceBox = styled(Grid2)({
+  height: "550px",
+  maxWidth: "1160px",
+  margin: "180px auto 170px",
   position: "relative",
-  padding: "32px 48px",
-  height: "100%",
-  color: "#fff",
+});
 
-  h2: {
-    fontSize: "24px",
-    fontWeight: 500,
-    lineHeight: "33.6px",
-    letterSpacing: "2px",
-    marginBottom: "24px",
-  },
+export const StyledOrderedList = styled(Box)({
+  padding: "32px 16px",
 
-  p: {
-    fontSize: "16px",
-    fontWeight: 400,
-    lineHeight: "24px",
-    letterSpacing: "1px",
-    maxWidth: "340px",
-    color: "rgba(255, 255, 255, 0.8)",
-    marginBottom: "40px",
-  },
+  ol: {
+    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+    gap: "16px",
+    paddingLeft: "16px",
+    listStyleType: "none",
+    counterReset: "list-counter",
+    maxWidth: 780,
 
-  button: {
-    border: "1px solid #1E7E9933",
-    padding: "10px 24px",
-    borderRadius: "40px",
-    color: "#fff",
-    fontSize: "16px",
-    fontWeight: 500,
-    lineHeight: "22.4px",
-    letterSpacing: "1px",
-  },
+    li: {
+      fontSize: "16px",
+      fontWeight: 400,
+      lineHeight: "24px",
+      fontFamily: "Manrope-Light",
+      marginLeft: "10px",
+      ...orderListBefore,
+      b: { color: "#00C3FF" },
+    },
 
-  ".profile-img": {
-    position: "absolute",
-    bottom: "0px",
-    right: "-20px",
-    objectFit: "cover",
-    borderRadius: "50%",
-    ...StyledImage,
+    ".work": {
+      strong: { color: "#FC04AD" },
+      cursor: "pointer",
+    },
+
+    ".platform": {
+      cursor: "pointer",
+      transition: "0.3s",
+
+      "&:hover": {
+        color: "#FC04AD",
+      },
+    },
   },
 });
 
-export const StyledOffersBox = styled(Grid2)({
-  ...styles,
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "space-between",
-  alignItems: "center",
-  paddingTop: "32px",
+// ExperianceBOx
 
-  h2: {
-    fontSize: "24px",
-    fontWeight: 500,
-    lineHeight: "36px",
-    letterSpacing: "1px",
-    color: "#fff",
-    marginBottom: "24px",
+export const StyledExperiance = styled(Box)({
+  position: "absolute",
+  color: "#fff",
+  minHeight: "230px",
+  backgroundColor: "#191919",
+  border: "1px solid #444444",
+
+  ".content": {
+    padding: "16px",
+    display: "flex",
+    gap: "32px",
+    backgroundColor: "#191919",
+    borderBottom: "1px solid #444444",
+    justifyContent: "space-between",
+
+    ".image-box": {
+      display: "flex",
+      alignItems: "center",
+      gap: "20px",
+    },
   },
 });
